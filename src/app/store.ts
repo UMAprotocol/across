68528578cc660bce545cd285961b9752104b28d1
Tulun <jaykiraly@gmail.com>
2021-09-02 13:59:07 -0700

    redux dev tools.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,11 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  devTools: true,
 });
 
 export type AppDispatch = typeof store.dispatch;
