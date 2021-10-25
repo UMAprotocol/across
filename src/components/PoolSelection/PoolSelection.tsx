404db9f9aecdcfeed60b28313d5609ecb2555c28
Tulun <jaykiraly@gmail.com>
2021-10-25 15:03:14 -0700

    cleanup unnec code.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/components/PoolSelection/PoolSelection.tsx
+++ src/components/PoolSelection/PoolSelection.tsx
@@ -1,15 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { ethers } from "ethers";
 import { useSelect } from "downshift";
 
-import {
-  useSend,
-  useBalances,
-  useConnection,
-  useBridgeFees,
-  useBlocks,
-} from "state/hooks";
-import { parseUnits, formatUnits, ParsingError, POOL_LIST } from "utils";
+import { useSend, useBalances, useConnection } from "state/hooks";
+import { formatUnits, POOL_LIST } from "utils";
 import { Section, SectionTitle } from "../Section";
 
 import {
@@ -21,15 +13,12 @@ import {
   ToggleButton,
   Logo,
   ToggleIcon,
-  ErrorBox,
 } from "./PoolSelection.styles";
 
 const PoolSelection = () => {
-  const [inputAmount, setInputAmount] = useState<string>("");
   const { account, isConnected } = useConnection();
-  const { setAmount, setToken, fromChain, toChain, amount } = useSend();
+  const { setToken, fromChain } = useSend();
 
-  const [error, setError] = React.useState<Error>();
   const { data: balances } = useBalances(
     {
       account: account!,
@@ -54,45 +43,6 @@ const PoolSelection = () => {
       }
     },
   });
-
-  useEffect(() => {
-    if (balances && amount && inputAmount !== "") {
-      const selectedIndex = POOL_LIST.findIndex(
-        ({ address }) => address === selectedItem!.address
-      );
-      const balance = balances[selectedIndex];
-      if (amount.lte(balance)) {
-        // clear the previous error if it is not a parsing error
-        setError((oldError) => {
-          if (oldError instanceof ParsingError) {
-            return oldError;
-          }
-          return undefined;
-        });
-      } else {
-        setError(new Error("Insufficient balance."));
-      }
-    }
-  }, [balances, amount, selectedItem, inputAmount]);
-
-  const { block } = useBlocks(toChain);
-
-  const { data: fees } = useBridgeFees(
-    {
-      amount,
-      tokenSymbol: selectedItem!.symbol,
-      blockNumber: block?.blockNumber ?? 0,
-    },
-    { skip: !isConnected || amount.lte(0) || !block }
-  );
-
-  const errorMsg = error
-    ? error.message
-    : fees?.isAmountTooLow
-    ? "Bridge fee is too high. Try sending a larger amount."
-    : undefined;
-
-  const showError = error || (fees?.isAmountTooLow && amount.gt(0));
 
   return (
     <Section>
@@ -127,7 +77,6 @@ const PoolSelection = () => {
               ))}
           </Menu>
         </InputGroup>
-        {showError && <ErrorBox>{errorMsg}</ErrorBox>}
       </Wrapper>
     </Section>
   );
