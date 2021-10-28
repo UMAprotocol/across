a5295f428ae5c385846767ec78d8489dac037a17
Tulun <jaykiraly@gmail.com>
2021-10-28 13:59:57 -0700

    fix merge conf.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/state/chainApi.ts
+++ src/state/chainApi.ts
@@ -1,5 +1,5 @@
 import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
-import { ethers,BigNumber } from "ethers";
+import { ethers, BigNumber } from "ethers";
 import { PROVIDERS, getRelayFees, ChainId, TOKENS_LIST, getLpFee } from "utils";
 import type { BridgeFees } from "utils";
 
@@ -38,7 +38,7 @@ const api = createApi({
           const provider = PROVIDERS[chainId]();
           const balances = await Promise.all(
             TOKENS_LIST[chainId].map(async (token) => {
-              try{
+              try {
                 // If it is ETH, use getBalance from the provider
                 if (token.symbol === "ETH") {
                   return provider.getBalance(account);
@@ -48,20 +48,16 @@ const api = createApi({
                   provider
                 );
                 return await contract.balanceOf(account);
-              }catch(err){
-                console.error(`Error fetching balance for: ${token.name} at ${token.address}`)
-                console.error(err)
-                return BigNumber.from('0')
+              } catch (err) {
+                console.error(
+                  `Error fetching balance for: ${token.name} at ${token.address}`
+                );
+                console.error(err);
+                return BigNumber.from("0");
               }
-              const contract = ERC20Ethers__factory.connect(
-                token.address,
-                provider
-              );
-              const balance = await contract.balanceOf(account);
-              return balance;
             })
           );
-          return { data:balances}
+          return { data: balances };
         } catch (error) {
           return { error };
         }
