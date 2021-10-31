fa2f57bc1706ed9761114cd50c665b38e60461ee
david <david@umaproject.org>
2021-10-30 21:09:20 -0400

    fix: fix onboard crashing due to re-init on tab select
    Signed-off-by: david <david@umaproject.org>

--- src/hooks/useOnboard.ts
+++ src/hooks/useOnboard.ts
@@ -1,54 +1,65 @@
-import React from "react";
+// import React from "react";
 import Onboard from "bnc-onboard";
-import { Wallet } from "bnc-onboard/dist/src/interfaces";
+import { Wallet, Initialization } from "bnc-onboard/dist/src/interfaces";
 import { ethers } from "ethers";
 import { onboardBaseConfig } from "utils";
-import { useConnection } from "state/hooks";
+import { update,disconnect,error } from "state/connection";
+import {store} from 'state'
 
-export function useOnboard() {
-  const { disconnect, setUpdate, setError } = useConnection();
+export type Emit = (event:string, data?:any) => void
+export function OnboardReact(config:Initialization, emit:Emit){
+  const onboard = Onboard({
+    ...config,
+    subscriptions: {
+      address: (address: string) => {
+        emit('update',{ account: address });
+      },
+      network: (chainIdInHex) => {
+        if (chainIdInHex == null) {
+          return;
+        }
+        const chainId = ethers.BigNumber.from(chainIdInHex).toNumber();
 
-  const instance = React.useMemo(
-    () =>
-      Onboard({
-        ...onboardBaseConfig(),
-        subscriptions: {
-          address: (address: string) => {
-            setUpdate({ account: address });
-          },
-          network: (chainIdInHex) => {
-            if (chainIdInHex == null) {
-              return;
-            }
-            const chainId = ethers.BigNumber.from(chainIdInHex).toNumber();
-
-            setUpdate({ chainId });
-          },
-          wallet: (wallet: Wallet) => {
-            const provider = new ethers.providers.Web3Provider(wallet.provider);
-            const signer = provider.getSigner();
-            setUpdate({
-              account: wallet.provider.selectedAddress,
-              provider,
-              signer,
-            });
-          },
-        },
-      }),
-    [setUpdate]
-  );
-
-  const init = React.useCallback(async () => {
-    try {
-      await instance.walletSelect();
-      await instance.walletCheck();
-    } catch (error: unknown) {
-      setError({ error: new Error("Could not initialize Onboard.") });
+        emit('update',{ chainId });
+      },
+      wallet: (wallet: Wallet) => {
+        const provider = new ethers.providers.Web3Provider(wallet.provider);
+        const signer = provider.getSigner();
+        emit('update',{
+          account: wallet.provider.selectedAddress,
+          provider,
+          signer,
+        });
+      },
+    },
+  })
+  async function init(){
+    try{
+    await onboard.walletSelect();
+    await onboard.walletCheck();
+      emit('init')
+    }catch(err:any){
+      emit('error',new Error("Could not initialize Onboard: " + err.message as string)) 
     }
-  }, [instance, setError]);
-  const reset = React.useCallback(() => {
-    instance.walletReset();
-    disconnect();
-  }, [instance, disconnect]);
-  return { init, reset };
+  }
+  async function reset(){
+    try{
+      await onboard.walletReset();
+      emit('disconnect')
+    }catch(err){
+      emit('error',err)
+    }
+  }
+  return { init, reset, onboard };
 }
+export const onboard = OnboardReact(onboardBaseConfig(),(event,data)=>{
+  if(event === 'update'){
+    store.dispatch(update(data))
+  }
+  if(event === 'disconnect'){
+    store.dispatch(disconnect())
+  }
+  if(event === 'error'){
+    store.dispatch(error(data))
+  }
+})
